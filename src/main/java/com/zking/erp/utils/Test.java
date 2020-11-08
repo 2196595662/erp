e7befa8929eb10103d096aff24f1575d0240b039
2196595662 <2196595662@qq.com>
2020-11-08 10:50:03 +0800

    dev

--- src/main/java/com/zking/erp/utils/Test.java
+++ src/main/java/com/zking/erp/utils/Test.java
@@ -3,5 +3,11 @@ package com.zking.erp.utils;
 public class Test {
     public static void main(String[] args) {
         System.out.println("我爱苏凡");
+
     }
+
+    public int add(int a,int b){
+        return a+b;
+    }
+
 }
